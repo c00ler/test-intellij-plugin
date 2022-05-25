543953deeeb688afc526cf15b4d42e503599812b
GitHub Action <action@github.com>
2022-05-25 07:41:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/c00ler/testintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/c00ler/testintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.c00ler.testintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.c00ler.testintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
